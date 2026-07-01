1b0a350e41f3ec57e5228eed530f64643d1de281
kay kay <kaykay5185@gmail.com>
2026-07-01 21:01:18 +0300

    my trean

--- sorce.js
+++ sorce.js
@@ -1,2 +1,45 @@
 document.getElementById("demo").innerHTML = "Hello JavaScript";
 
+let btn_on = document.getElementById("btn_on")
+let btn_off = document.getElementById("btn_off")
+// let img = document.getElementById("myimg")
+
+btn_on.addEventListener("click", img_on)
+btn_off.addEventListener("click", img_off)
+btn_volume.addEventListener("click", img_volume)
+
+function img_on (){
+
+    myimg.src = "https://www.w3schools.com/js/pic_bulbon.gif"
+    // btn_on.style.backgroundColor = "green"
+
+    btn_on.classList.add("btn_on", "gray")
+    btn_off.classList.remove("btn_on", "gray")
+    
+    // myimg.style.border = "2px solid green"
+
+}
+
+function img_off(){
+
+    myimg.src = "https://www.w3schools.com/js/pic_bulboff.gif"
+    // btn_on.style.backgroundColor =  "white"
+    btn_on.classList.remove("btn_on", "gray")
+    btn_off.classList.add("btn_on", "gray")
+}
+
+function img_volume(){
+    // clone_lamp = lamp.cloneNode(true)
+    // clone_lamp.id = "clone_lamp"
+    // lampRow.appendChild(clone_lamp)
+    // lamp.after(clone_lamp)
+    nr = document.getElementById("myRange").value
+    for (let i = 0; i < nr; i++){
+        clone_lamp = lamp.cloneNode(true)
+        clone_lamp.id = "clone_lamp"
+        lamp.after(clone_lamp) 
+    }
+
+}
+
+
